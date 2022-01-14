aa84f70ba43881dc00f67d6c286aaf80808ffebd
Gangula2 <gangulaanwesh@gmail.com>
2022-01-15 02:48:29 +0530

    use SVG icon for heading link on hover

--- utils/mdx.js
+++ utils/mdx.js
@@ -7,6 +7,7 @@ import rehypeSlug from 'rehype-slug';
 import rehypeCodeTitles from 'rehype-code-titles';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypePrism from 'rehype-prism-plus';
+import { s } from 'hastscript'
 
 export const POSTS_PATH = path.join(process.cwd(), "data/posts");
 
@@ -44,6 +45,14 @@ export const getSinglePost = async (slug) => {
                 [
                     rehypeAutolinkHeadings,
                     {
+                        content: s(
+                            // add SVG using rehype-autolink-headings in mdx.js - https://github.com/remarkjs/remark/discussions/732#discussioncomment-816042
+                            // another reference: https://github.com/janosh/svelte-toc/commit/8493df334a11661eddf03434372f6cd71ea313c1
+                            `svg`,
+                            { width: 16, height: 16, viewBox: `0 0 16 16` },
+                            // symbol #link-icon defined in app.html
+                            s(`use`, { 'xlink:href': `#link-icon` })
+                        ),
                         properties: {
                             className: ['rehypeautolinkheadings'], // TODO: this is not working with CSS Module imports (since module imports rename classes)
                         },
